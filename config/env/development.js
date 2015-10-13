42ae2ed1b5e9683e024f9b17d2efedc5941c1f19
Julio Cesar Mosqueda <jcmosqued@hotmail.com>
2015-10-13 13:22:41 -0500

    Commit inicial

---
+++ config/env/development.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+	db: 'mongodb://localhost/quatroperedes-dev',
+	app: {
+		title: 'Quatroparedes'
+	},
+	facebook: {
+		clientID: process.env.FACEBOOK_ID || '144177522581552',
+		clientSecret: process.env.FACEBOOK_SECRET || '96d44b67c0fdd8d2953166b9756b94a9',
+		callbackURL: '/auth/facebook/callback'
+	},
+	twitter: {
+		clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
+		clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
+		callbackURL: '/oauth2callback'
+	},
+	google: {
+		clientID: process.env.GOOGLE_ID || '50521223582-6bbrjlnjmdc4kdpglqhl6b94acqtcgvd.apps.googleusercontent.com',
+		clientSecret: process.env.GOOGLE_SECRET || 'x95YMBQfai0EuE9hEZC2WJuu',
+		callbackURL: '/auth/google/callback'
+	},
+	linkedin: {
+		clientID: process.env.LINKEDIN_ID || 'APP_ID',
+		clientSecret: process.env.LINKEDIN_SECRET || 'APP_SECRET',
+		callbackURL: '/auth/linkedin/callback'
+	},
+	github: {
+		clientID: process.env.GITHUB_ID || 'APP_ID',
+		clientSecret: process.env.GITHUB_SECRET || 'APP_SECRET',
+		callbackURL: '/auth/github/callback'
+	},
+	mailer: {
+		from: process.env.MAILER_FROM || 'remotecnologias',
+		options: {
+			service: process.env.MAILER_SERVICE_PROVIDER || 'google',
+			auth: {
+				user: process.env.MAILER_EMAIL_ID || 'remotecnologias@gmail.com',
+				pass: process.env.MAILER_PASSWORD || 'ng05jc28'
+			}
+		}
+	}
+};
---
+++ config/env/production.js
@@ -0,0 +1,67 @@
+'use strict';
+
+module.exports = {
+	db: 'mongodb://localhost/quatroparedes-dev',
+	assets: {
+		lib: {
+			css: [
+				//'public/lib/bootstrap/dist/css/bootstrap.min.css',
+				//'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+			],
+			js: [
+				'public/lib/angular/angular.min.js',
+				'public/lib/angular-resource/angular-resource.min.js', 
+				//'public/lib/angular-cookies/angular-cookies.js', 
+				//'public/lib/angular-animate/angular-animate.js', 
+				//'public/lib/angular-touch/angular-touch.js', 
+				//'public/lib/angular-sanitize/angular-sanitize.js', 
+				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+				'public/lib/angular-ui-utils/ui-utils.min.js',
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+				'public/lib/jquery/dist/jquery.min.js',
+				'public/lib/bootstrap/dist/js/bootstrap.min.js',
+	            'public/lib/angular-base64-upload/dist/angular-base64-upload.min.js'
+	            
+
+			]
+		},
+		css: 'public/dist/application.min.css',
+		js:  ['public/dist/application.min.js',
+			 'http://maps.google.com/maps?file=api&amp;v=2&amp;key=ABQIAAAAg_2UIEV5rd9OVgNSGNfsyxTaN__vuAQAUAfJwnfsk7h_mauWkxSYbIJkdkwH1e7uYk7faoLIELG-Tw']
+	},
+	facebook: {
+		clientID: process.env.FACEBOOK_ID || '144177522581552',
+		clientSecret: process.env.FACEBOOK_SECRET || '96d44b67c0fdd8d2953166b9756b94a9',
+		callbackURL: '/auth/facebook/callback'
+	},
+	twitter: {
+		clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
+		clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
+		callbackURL: '/auth/twitter/callback'
+	},
+	google: {
+		clientID: process.env.GOOGLE_ID || '50521223582-6bbrjlnjmdc4kdpglqhl6b94acqtcgvd.apps.googleusercontent.com',
+		clientSecret: process.env.GOOGLE_SECRET || 'x95YMBQfai0EuE9hEZC2WJuu',
+		callbackURL: '/auth/google/callback'
+	},
+	linkedin: {
+		clientID: process.env.LINKEDIN_ID || 'APP_ID',
+		clientSecret: process.env.LINKEDIN_SECRET || 'APP_SECRET',
+		callbackURL: '/auth/linkedin/callback'
+	},
+	github: {
+		clientID: process.env.GITHUB_ID || 'APP_ID',
+		clientSecret: process.env.GITHUB_SECRET || 'APP_SECRET',
+		callbackURL: '/auth/github/callback'
+	},
+	mailer: {
+		from: process.env.MAILER_FROM || 'MAILER_FROM',
+		options: {
+			service: process.env.MAILER_SERVICE_PROVIDER || 'MAILER_SERVICE_PROVIDER',
+			auth: {
+				user: process.env.MAILER_EMAIL_ID || 'MAILER_EMAIL_ID',
+				pass: process.env.MAILER_PASSWORD || 'MAILER_PASSWORD'
+			}
+		}
+	}
+};
